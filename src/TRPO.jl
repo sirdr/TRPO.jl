d1a390732c5b48970f1fae701d4a3939387c6033
Loren AC <loren.k.ac@gmail.com>
2018-12-01 08:53:26 -0800

    adds beginning source files

---
+++ src/TRPO.jl
@@ -0,0 +1,35 @@
+module TRPO
+
+using Random
+using StatsBase
+using Printf
+using Parameters
+using Flux
+using BSON
+using POMDPs
+using POMDPModelTools
+using POMDPPolicies
+using RLInterface
+
+export TRPOSolver,
+       AbstractNNPolicy,
+       NNPolicy,
+       TRPOExperience,
+    
+       # helpers
+       flattenbatch,
+       huber_loss,
+       isrecurrent,
+       batch_trajectories
+
+include("helpers.jl")
+include("policy.jl")
+include("exploration_policy.jl")
+include("evaluation_policy.jl")
+include("experience_replay.jl")
+include("prioritized_experience_replay.jl")
+include("episode_replay.jl")
+include("dueling.jl")
+include("solver.jl")
+
+end # module DeepQLearning
---
+++ src/policy.jl
@@ -0,0 +1,56 @@
+abstract type AbstractNNPolicy  <: Policy end
+
+struct NNPolicy{P <: Union{MDP, POMDP}, Q, A} <: AbstractNNPolicy 
+    problem::P
+    network::Q
+    action_map::Vector{A}
+    n_input_dims::Int64
+end
+
+function reset!(policy::NNPolicy)
+    Flux.reset!(policy.network)
+end
+
+function _action(policy::NNPolicy{P,Q,A}, o::AbstractArray{T, N}) where {P<:Union{MDP,POMDP},Q,A,T<:Real,N}
+    if ndims(o) == policy.n_input_dims
+        obatch = reshape(o, (size(o)...,1))
+        vals = policy.network(obatch)
+        return policy.action_map[argmax(vals)]
+    else 
+        throw("NNPolicyError: was expecting an array with $(policy.n_input_dims) dimensions, got $(ndims(o))")
+    end
+end
+
+function POMDPPolicies.actionvalues(policy::NNPolicy{P,Q,A}, o::AbstractArray{T,N}) where {P<:Union{MDP,POMDP},Q,A,T<:Real,N}
+    if ndims(o) == policy.n_input_dims
+        obatch = reshape(o, (size(o)...,1))
+        return policy.network(obatch)
+    else 
+        throw("NNPolicyError: was expecting an array with $(policy.n_input_dims) dimensions, got $(ndims(o))")
+    end
+end
+
+function _value(policy::NNPolicy{P}, o::AbstractArray{T,N}) where {P<:Union{MDP,POMDP},T<:Real,N}
+    if ndims(o) == policy.n_input_dims
+        obatch = reshape(o, (size(o)...,1))
+        return maximum(policy.network(obatch))
+    else 
+        throw("NNPolicyError: was expecting an array with $(policy.n_input_dims) dimensions, got $(ndims(o))")
+    end
+end
+
+function POMDPs.action(policy::NNPolicy{P}, s) where {P <: MDP}
+    _action(policy, convert_s(Array{Float64}, s, policy.problem))
+end
+
+function POMDPs.action(policy::NNPolicy{P}, o) where {P <: POMDP}
+    _action(policy, convert_o(Array{Float64}, o, policy.problem))
+end
+
+function POMDPs.value(policy::NNPolicy{P}, s) where {P <: MDP}
+    _value(policy, convert_s(Array{Float64}, s, policy.problem))
+end
+
+function POMDPs.value(policy::NNPolicy{P}, o) where {P <: POMDP}
+    _value(policy, convert_o(Array{Float64}, o, policy.problem))
+end
