e93628225fe70081cb2fc887dab9b824b0a8bea9
Loren AC <loren.k.ac@gmail.com>
2018-12-06 12:36:05 -0800

    bug fixes

--- src/trpo_step.jl
+++ src/trpo_step.jl
@@ -92,7 +92,7 @@ function trpo_step(model, get_loss, get_kl, max_kl, damping)
         append!(flat_grads_loss, reshape(g, length(g)))
     end
 
-    print(flat_grads_loss)
+    print(loss)
 
     function fisher_vector_product(v)
         kl = get_kl()
