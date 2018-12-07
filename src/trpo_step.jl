195077cad81fe13df8c4bf53f01e1e0c26523279
Loren AC <loren.k.ac@gmail.com>
2018-12-07 01:20:12 -0800

    bug fixes

--- src/trpo_step.jl
+++ src/trpo_step.jl
@@ -94,58 +94,57 @@ function trpo_step(model, get_loss, get_kl, max_kl, damping, get_fim)
 
     function fisher_vector_product(v)
 
-        if get_fim == nothing
-            # Use direct method (Currently not supported in Julia)
-            kl = get_kl(model)
-            kl = mean(kl)
+        # if get_fim == nothing
+        #     # Use direct method (Currently not supported in Julia)
+        #     kl = get_kl(model)
+        #     kl = mean(kl)
 
-            # maybe make this a function if possible
-            flat_grads = []
-            for param in params(model)
-                grads = Tracker.gradient(() -> kl, Params(param))
-                g = grads[param]
-                append!(flat_grads, reshape(g, length(g)))
-            end
+        #     # maybe make this a function if possible
+        #     flat_grads = []
+        #     for param in params(model)
+        #         grads = Tracker.gradient(() -> kl, Params(param))
+        #         g = grads[param]
+        #         append!(flat_grads, reshape(g, length(g)))
+        #     end
 
-            kl_v = sum(flat_grads.*v)
+        #     kl_v = sum(flat_grads.*v)
 
-            flat_grads_grads_kl = Float64[]
-            for param in params(model)
-                grads = Tracker.gradient(() -> kl_v, Params(param))
-                g = Tracker.data(grads[param])
-                append!(flat_grads_grads_kl, reshape(g, length(g)))
-            end
+        #     flat_grads_grads_kl = Float64[]
+        #     for param in params(model)
+        #         grads = Tracker.gradient(() -> kl_v, Params(param))
+        #         g = Tracker.data(grads[param])
+        #         append!(flat_grads_grads_kl, reshape(g, length(g)))
+        #     end
 
-            return flat_grads_grads_kl .+ v .* damping
-        else
-            fim, action_probs = get_fim(model)
-            t = ones(size(action_probs))
-            at = sum(action_probs .* param(t))
+        #     return flat_grads_grads_kl .+ v .* damping
+        # else
+        fim, action_probs = get_fim(model)
+        t = ones(size(action_probs))
+        at = sum(action_probs .* param(t))
 
-            Jt = []
-            for param in params(model)
-                grads = Tracker.gradient(() -> at, Params(param))
-                g = grads[param]
-                append!(Jt, reshape(g, length(g)))
-            end
-
-            Jtv = sum(Jt .* v)
-            Jv = Tracker.gradient(() -> Jtv, Params(t))
-
-            print(size(Jv))
-            print(size(fim))
-
-            MJv = fim .* Tracker.data(Jv)
-            a_MJv = sum(action_probs .* MJv)
-
-            JTMJv = []
-            for param in params(model)
-                grads = Tracker.gradient(() -> a_MJv, Params(param))
-                g = Tracker.data(grads[param])
-                append!(JTMJv, reshape(g, length(g)))
-            end
-            return JTMJv .+ v .* damping
+        Jt = []
+        for param in params(model)
+            grads = Tracker.gradient(() -> at, Params(param))
+            g = grads[param]
+            append!(Jt, reshape(g, length(g)))
         end
+
+        Jtv = sum(Jt .* v)
+        Jv = Tracker.gradient(() -> Jtv, Params(t))
+
+        print(size(Jv))
+        print(size(fim))
+
+        MJv = fim .* Tracker.data(Jv)
+        a_MJv = sum(action_probs .* MJv)
+
+        JTMJv = []
+        for param in params(model)
+            grads = Tracker.gradient(() -> a_MJv, Params(param))
+            g = Tracker.data(grads[param])
+            append!(JTMJv, reshape(g, length(g)))
+        end
+        return JTMJv .+ v .* damping
     end
 
     step_direction = conjugate_gradients(fisher_vector_product, -1 .*flat_grads_loss, 10)
